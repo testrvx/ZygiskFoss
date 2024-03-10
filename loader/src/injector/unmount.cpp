9c5baba315d66f00a23b4b0c7a800d0912b37cee
Admire <admire1971@outlook.com>
2024-03-10 16:38:09 +0800

    ZygiskNext: Initial Support for APatch
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- loader/src/injector/unmount.cpp
+++ loader/src/injector/unmount.cpp
@@ -11,6 +11,8 @@ using namespace std::string_view_literals;
 namespace {
     constexpr auto MODULE_DIR = "/data/adb/modules";
     constexpr auto KSU_OVERLAY_SOURCE = "KSU";
+    constexpr auto APATCH_OVERLAY_SOURCE = "APatch";
+    constexpr auto ZYGISK_FUSE_SOURCE = "zygisk";
     const std::vector<std::string> KSU_PARTITIONS{"/system", "/vendor", "/product", "/system_ext", "/odm", "/oem"};
 
     void lazy_unmount(const char* mountpoint) {
@@ -50,7 +52,15 @@ void revert_unmount_ksu() {
         if (info.type == "tmpfs" && info.source == KSU_OVERLAY_SOURCE) {
             targets.emplace_back(info.target);
         }
-    }
+       // Unmount /debug_ramdisk
+        if (info.target.starts_with("/debug_ramdisk")) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount fuse
+        if (info.type == "fuse" && info.source == ZYGISK_FUSE_SOURCE) {
+            targets.emplace_back(info.target);
+        }
+  }
     for (auto& info: parse_mount_info("self")) {
         // Unmount everything from ksu loop except ksu module dir
         if (info.source == ksu_loop && info.target != MODULE_DIR) {
@@ -80,6 +90,50 @@ void revert_unmount_magisk() {
         }
     }
 
+    for (auto& s: reversed(targets)) {
+        lazy_unmount(s.data());
+    }
+}
+void revert_unmount_apatch() {
+      std::string apatch_loop;
+    std::vector<std::string> targets;
+
+    // Unmount apatch module dir last
+    targets.emplace_back(MODULE_DIR);
+
+
+    for (auto& info: parse_mount_info("self")) {
+        if (info.target == MODULE_DIR) {
+            apatch_loop = info.source;
+            continue;
+        } 
+        // Unmount everything mounted to /data/adb
+        if (info.target.starts_with("/data/adb")) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount APATCH overlays
+        if (info.type == "overlay"
+            && info.source == APATCH_OVERLAY_SOURCE
+            && std::find(KSU_PARTITIONS.begin(), KSU_PARTITIONS.end(), info.target) != KSU_PARTITIONS.end()) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount /debug_ramdisk
+        if (info.target.starts_with("/debug_ramdisk")) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount fuse
+        if (info.type == "fuse" && info.source == ZYGISK_FUSE_SOURCE) {
+            targets.emplace_back(info.target);
+        }
+    }
+    for (auto& info: parse_mount_info("self")) {
+        // Unmount everything from apatch loop except apatch module dir
+        if (info.source == apatch_loop && info.target != MODULE_DIR) {
+            targets.emplace_back(info.target);
+        }
+    }
+
+    // Do unmount
     for (auto& s: reversed(targets)) {
         lazy_unmount(s.data());
     }
