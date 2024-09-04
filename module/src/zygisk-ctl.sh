dac9f72f191e0e33501e637f193b261367d8f7bd
DanGLES3 <switchtegra@gmail.com>
2024-09-04 04:42:07 -0300

    Update zygisk-ctl.sh

--- module/src/zygisk-ctl.sh
+++ module/src/zygisk-ctl.sh
@@ -1,6 +1,6 @@
 MODDIR=${0%/*}/..
 
 export TMP_PATH=/sbin
-[ -d /sbin ] || export TMP_PATH=/debug_ramdisk
+[ -d /sbin ] || export TMP_PATH=/data/adb/zygisksu
 
 exec $MODDIR/bin/zygisk-ptrace64 ctl $*
